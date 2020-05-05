72cb9cb805a56b69acca4b68d87f1734f9f1e952
Mario McGee <mario.mcgee@students.makeschool.com>
2020-05-04 20:27:46 -0700

    fixed tests.py errors

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -24,15 +24,12 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
-    def create_question(question_text, days):
-        time = timezone.now() + datetime.timedelta(days=days)
-        return Question.objects.create(question_text=question_text, pub_date=time)
 
     
     
-    
-
-
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 
