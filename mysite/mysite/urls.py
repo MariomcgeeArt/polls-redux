2a30dc42f01d04a6c995e52cdeac34971cd69672
Mario McGee <mario.mcgee@students.makeschool.com>
2020-05-04 20:18:06 -0700

    fixing testing errors

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.urls import include, path
+import polls.urls
+
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('', include('polls.urls')),
     path('admin/', admin.site.urls), 
 ]
---
+++ mysite/no.py
@@ -0,0 +1,22 @@
+
+
+wendy = ['hello','there', 'time']
+
+
+
+def if_practice(wendy):
+    
+    if wendy == 0:
+        
+        return print('it is')
+    else:
+        return print('it is not')
+
+def print_array(list):
+
+    for word in list:
+        print(word[1])
+
+
+
+print_array(wendy)
